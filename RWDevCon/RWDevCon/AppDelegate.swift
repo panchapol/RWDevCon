741c234c1e982f40452d7a828ea2e611e1470c0a
Pop Panchapol <panchapol@gmail.com>
2015-10-20 11:45:16 +0700

    Init project

---
+++ RWDevCon/RWDevCon/AppDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  AppDelegate.swift
+//  RWDevCon
+//
+//  Created by Pop Panchapol on 10/20/15.
+//  Copyright © 2015 Pop Panchapol. All rights reserved.
+//
+
+import UIKit
+import TVMLKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDelegate {
+
+    var window: UIWindow?
+
+    var appController: TVApplicationController?
+    static let TVBaseURL = "http://localhost:9003/"
+    static let TVBootURL = "\(AppDelegate.TVBaseURL)js/application.js"
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        let appControllerContext = TVApplicationControllerContext()
+        
+        guard let javaScriptURL = NSURL(string: AppDelegate.TVBootURL) else {
+            fatalError("unable to create NSURL")
+        }
+        appControllerContext.javaScriptApplicationURL = javaScriptURL
+        appControllerContext.launchOptions["BASEURL"] = AppDelegate.TVBaseURL
+        
+        appController = TVApplicationController(context: appControllerContext, window: window, delegate: self)
+        return true
+    }
+}
+
